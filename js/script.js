cd3fb1e28e49208de675f4c52062d4aee798adb0
hat <jburns@chapman.edu>
2018-10-24 22:05:57 -0700

    Finished design

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
 function  open_resume() {
     window.open('public/NOT_resume.pdf');
 }
+
+particlesJS.load('particles-js', 'js/particles.json', function() {
+    console.log('Particles.js config loaded');
+});
