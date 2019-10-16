33ff86b490fe8ecc9bc93d29407ade5541963d46
jhburns <jburns@chapman.edu>
2019-10-15 19:23:32 -0700

    Updating to networking specs

--- js/script.js
+++ js/script.js
@@ -2,6 +2,10 @@ function  open_resume() {
     window.open('public/NOT_resume.pdf');
 }
 
+function  open_real() {
+    window.open('public/Resume.pdf');
+}
+
 particlesJS.load('particles-js', 'js/particles.json', function() {
     console.log('Particles.js config loaded');
 });
